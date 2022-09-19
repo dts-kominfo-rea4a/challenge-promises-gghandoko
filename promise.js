0729aa651b9d52aded8278cd7f2151ead5a1a426
gghandoko <requireapps@gmail.com>
2022-09-19 20:56:02 +0700

    push movie

--- promise.js
+++ promise.js
@@ -1,7 +1,38 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = ;
+
+const promiseOutput = (dataMovie) => {
+  // console.log(dataMovie);
+
+  return new Promise((resolve) => {
+    (async () => {
+      var hasil1 = await promiseTheaterIXX();
+      var hasil2 = await promiseTheaterVGC();
+
+      // console.log(hasil1);
+      // console.log(hasil2);
+      calculate = 0;
+
+      for (tempCal of hasil1) {
+        // console.log(tempCal);
+        if (tempCal['hasil'] == dataMovie) {
+          calculate++;
+        }
+      }
+
+      for (tempCal2 of hasil2) {
+        if (tempCal2['hasil'] == dataMovie) {
+          calculate++;
+        }
+      }
+      resolve(calculate);
+    })();
+    
+  });
+}
+
 
 module.exports = {
   promiseOutput,
